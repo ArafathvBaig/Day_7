d44fdc916940a069e2c2aaedcce0832c2437a829
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-29 10:16:23 +0530

    Added: Gambling.java File

---
+++ src/com/Day_7/Gambling.java
@@ -0,0 +1,12 @@
+package com.Day_7;
+
+public class Gambling 
+{
+	public static int DAILY_LIMIT=100;
+	public static int PER_GAME_LIMIT=1;
+	public static void main(String[] args) 
+	{
+		System.out.println(+DAILY_LIMIT);
+		System.out.println(+PER_GAME_LIMIT);
+	}
+}
