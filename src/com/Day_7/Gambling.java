d203c39c8b0a4b03675f4415667e8beaae6cf600
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-29 10:58:38 +0530

    Updated: Gambling.java File with UC2

--- src/com/Day_7/Gambling.java
+++ src/com/Day_7/Gambling.java
@@ -4,9 +4,23 @@ public class Gambling
 {
 	public static int DAILY_LIMIT=100;
 	public static int PER_GAME_LIMIT=1;
+	public static int WIN=1;
+	public static int loss=0;
 	public static void main(String[] args) 
 	{
+		int check;
+		check = (int) (Math.random()*10)%2;
+		System.out.println("Check:: "+check);
+		if(check==WIN)
+		{
+			System.out.print("Won: ");
+			DAILY_LIMIT++;
+		}
+		else
+		{
+			System.out.print("Lost: ");
+			DAILY_LIMIT--;
+		}
 		System.out.println(+DAILY_LIMIT);
-		System.out.println(+PER_GAME_LIMIT);
 	}
 }
