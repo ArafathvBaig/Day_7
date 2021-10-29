b24b35dc0a17e7aee3f32211b7e33d08a43ae06f
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-29 12:23:39 +0530

    Updated: Gambling.java File With UC4

--- src/com/Day_7/Gambling.java
+++ src/com/Day_7/Gambling.java
@@ -4,41 +4,65 @@ public class Gambling
 {
 	public static final int  DAILY_LIMIT=100;
 	public static final int PER_GAME_LIMIT=1;
+	public static final int MAX_DAYS=20;
 	public static final int WIN=1;
 	public static final int LOSS=0;
 	public static void main(String[] args) 
 	{
 		int check;
+		int day=1;
 		int profit;
 		int lost;
-		int daily_limit=DAILY_LIMIT;
-		System.out.println(+daily_limit);
-		while((daily_limit>50) && (daily_limit<150))
+		int total=DAILY_LIMIT*MAX_DAYS;
+		while(day<=MAX_DAYS)
 		{
-			check = (int) (Math.random()*10)%2;
-			System.out.println("Check:: "+check);
-			if(check==WIN)
+			int daily_limit=DAILY_LIMIT;
+			System.out.println(+daily_limit);
+			while((daily_limit>50) && (daily_limit<150))
 			{
-				System.out.print("Won: ");
-				daily_limit++;
-				System.out.println(+daily_limit);
+				check = (int) (Math.random()*10)%2;
+				System.out.println("Check:: "+check);
+				if(check==WIN)
+				{
+					System.out.print("Won: ");
+					daily_limit++;
+					System.out.println(+daily_limit);
+				}
+				else
+				{
+					System.out.print("Lost: ");
+					daily_limit--;
+					System.out.println(+daily_limit);
+				}
+			}
+			System.out.println("DAY:: "+day);
+			if(daily_limit>100)
+			{
+				profit=daily_limit-100;
+				total=total+profit;
+				System.out.println("DAY "+day+" has Profit of :: "+profit);
 			}
 			else
 			{
-				System.out.print("Lost: ");
-				daily_limit--;
-				System.out.println(+daily_limit);
+				lost=100-daily_limit;
+				total=total-lost;
+				System.out.println("DAY "+day+" has Loss of :: "+lost);
 			}
+			day++;
 		}
-		if(daily_limit>DAILY_LIMIT)
+		if(total>DAILY_LIMIT*MAX_DAYS)
 		{
-			profit=daily_limit-DAILY_LIMIT;
-			System.out.println("Profit of the Day: "+profit);
+			profit=total-(DAILY_LIMIT*MAX_DAYS);
+			System.out.println("Month has a profit of: "+profit);
+		}
+		else if(total<DAILY_LIMIT*MAX_DAYS)
+		{
+			lost=(DAILY_LIMIT*MAX_DAYS)-total;
+			System.out.println("Month has a profit of: "+lost);
 		}
 		else
 		{
-			lost=DAILY_LIMIT-daily_limit;
-			System.out.println("Loss of the Day: "+lost);
+			System.out.println("No Profit or No Loss");
 		}
 	}
 }
