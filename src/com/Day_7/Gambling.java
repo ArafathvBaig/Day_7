57502b190656a263798c8fd71fb6348c4151c184
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-29 12:16:35 +0530

    Updated: Gambling.java File WIth UC3

--- src/com/Day_7/Gambling.java
+++ src/com/Day_7/Gambling.java
@@ -2,25 +2,43 @@ package com.Day_7;
 
 public class Gambling 
 {
-	public static int DAILY_LIMIT=100;
-	public static int PER_GAME_LIMIT=1;
-	public static int WIN=1;
-	public static int LOSS=0;
+	public static final int  DAILY_LIMIT=100;
+	public static final int PER_GAME_LIMIT=1;
+	public static final int WIN=1;
+	public static final int LOSS=0;
 	public static void main(String[] args) 
 	{
 		int check;
-		check = (int) (Math.random()*10)%2;
-		System.out.println("Check:: "+check);
-		if(check==WIN)
+		int profit;
+		int lost;
+		int daily_limit=DAILY_LIMIT;
+		System.out.println(+daily_limit);
+		while((daily_limit>50) && (daily_limit<150))
 		{
-			System.out.print("Won: ");
-			DAILY_LIMIT++;
+			check = (int) (Math.random()*10)%2;
+			System.out.println("Check:: "+check);
+			if(check==WIN)
+			{
+				System.out.print("Won: ");
+				daily_limit++;
+				System.out.println(+daily_limit);
+			}
+			else
+			{
+				System.out.print("Lost: ");
+				daily_limit--;
+				System.out.println(+daily_limit);
+			}
+		}
+		if(daily_limit>DAILY_LIMIT)
+		{
+			profit=daily_limit-DAILY_LIMIT;
+			System.out.println("Profit of the Day: "+profit);
 		}
 		else
 		{
-			System.out.print("Lost: ");
-			DAILY_LIMIT--;
+			lost=DAILY_LIMIT-daily_limit;
+			System.out.println("Loss of the Day: "+lost);
 		}
-		System.out.println(+DAILY_LIMIT);
 	}
 }
